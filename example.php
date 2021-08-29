243d829007abebd66dce1948ad9d473674e74028
charbel2002 <55005228+charbel2002@users.noreply.github.com>
2021-08-29 01:59:34 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,25 +1,29 @@
 <?php
 
-    require("path/to/controler.php");
+    require("path/to/controller.php");
 
     //we initialize some variables
 
-    $log=null;
-    $response=null;
+    $log = null;
+    $response = null;
+
+    $statement = "";
 
     //You can add your own necessary variables
 
     //here we make a simple implementation of the logManager class
 
-    $call=new logManager("credentials");//add your own dbname here instead of credential, mine is credentials
+    $call = new logManager("credentials");//add your own dbname here instead of credential, mine is credentials
 
     $call->accessLayer($response);//You need to call this function first, otherwise the controler will throw error
 
-    if($response["status"]==200){
+    if($response["status"] == 200){
 
       $call->loginServ($log,$response);
 
         //you can now try using an additionnal function you've been added to the controler
+        
+        //Assume it's : $call->register($log,$statement)
 
       $call->logoutServe($log);
 
